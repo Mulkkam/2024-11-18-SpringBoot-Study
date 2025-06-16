b6c3d279f7e0789d496e3994a38ccc5159681fc3
SIST-112 <SIST-112@DESKTOP-MAT3KCP>
2025-06-16 14:02:16 +0900

    2025-06-16

---
+++ devops/src/main/java/com/sist/web/vo/BoardVO.java
@@ -0,0 +1,12 @@
+package com.sist.web.vo;
+
+public interface BoardVO {
+   public int getNo();
+   public String getName();
+   public String getSubject();
+   public String getContent();
+   public String getDbday();
+   public int getHit();
+   public int getNum();
+   
+}
